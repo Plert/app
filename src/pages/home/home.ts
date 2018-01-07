5439a3cdcd6516223d947979feb828588a2d0e3e
Eduardo Salazar <esalazar922@gmail.com>
2018-01-07 20:09:25 +0800

    fixed sms

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -64,7 +64,7 @@ export class HomePage {
   sendAlert(alert){
     // Send message
     let newMessage = alert.message + "\n(This is an automatic alert - Visit www.plert.com)"
-    this.sms.sendSMS([alert.phone1,alert.phone2,alert.phone3],alert.message);
+    this.sms.sendSMS([alert.phone1,alert.phone2,alert.phone3],newMessage);
   }
 
   start(){
